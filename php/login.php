e767ede5169f749f28ae6f5717cefb215b4dae67
Mariia Borowiec <syrotamaria@gmail.com>
2018-12-21 23:35:53 +0100

    Refaktoryzacja logowania. Wyjątki. Metody.

--- php/login.php
+++ php/login.php
@@ -7,82 +7,89 @@
     }
 
     require_once "connect.php";
+    mysqli_report(MYSQLI_REPORT_STRICT);
 
-    $db_connection = @new mysqli($host, $db_user, $db_password, $db_name);
-    if ($db_connection->connect_errno != 0)
-	{
-		echo "Błąd: ".$db_connection->connect_errno." Skontaktuj się z Administratorem";
-	}
-	else
-	{
-        $login = $_POST['emailLogin'];
-        $password = $_POST['passwordLogin'];
-
-        $login = htmlentities($login, ENT_QUOTES, "UTF-8");
-
-        if ($result = @$db_connection->query(
-            sprintf("SELECT * FROM userlogged ul JOIN user u ON u.id = ul.id_user WHERE u.email='%s'",
-                    mysqli_real_escape_string($db_connection, $login))))
-        {
-            $user_logged_count = $result->num_rows;
-            if ($user_logged_count > 0)
-            {
-                $row = $rezultat->fetch_assoc();
-                if (password_verify($password, $row['password']))
-                {
-                    $_SESSION['isUserLogged'] = true;
-                    $_SESSION['id'] = $row['ul.id'];
-                    $_SESSION['surname'] = $wiersz['surname'];
-                    $_SESSION['name'] = $wiersz['name'];
-                    $_SESSION['email'] = $wiersz['email'];
-                    unset($_SESSION['error']);
-                    $result->free_result();
-                    header('Location: userLogged.html');
-                }
-                else
-                {
-                    $_SESSION['error'] = '<span style="color:red">Wprowadzono nieprawidłowy login lub hasło!</span>';
-                    header('Location: ../index.php');
-                }
-            }
-            else
-            {
-                $_SESSION['error'] = '<span style="color:red">Wprowadzono nieprawidłowy login lub hasło!</span>';
-                header('Location: ../index.php');
-            }
-        }
+    try {
+        $db_connection = new mysqli($host, $db_user, $db_password, $db_name);
+        if ($db_connection->connect_errno != 0)
+            throw new Exception(mysqli_connect_errno());
         else
-        if ($result = @$db_connection->query(
-            sprintf("SELECT * FROM admin a JOIN user u ON u.id = a.id_user WHERE u.email='%s'",
-                    mysqli_real_escape_string($db_connection, $login))))
         {
-            $user_logged_count = $result->num_rows;
-            if ($user_logged_count > 0)
-            {
-                $row = $rezultat->fetch_assoc();
-                if (password_verify($password, $row['password']))
+            $login = $_POST['emailLogin'];
+            $password = $_POST['passwordLogin'];
+
+            $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+
+            if ($result = $db_connection->query(
+                sprintf("SELECT * FROM userlogged ul JOIN user u ON u.id = ul.id_user WHERE u.email='%s'",
+                        mysqli_real_escape_string($db_connection, $login))))
+                loginUserLogged();
+            else
+            if ($result = $db_connection->query(
+                sprintf("SELECT * FROM admin a JOIN user u ON u.id = a.id_user WHERE u.email='%s'",
+                        mysqli_real_escape_string($db_connection, $login))))
+                loginAdmin();
+
+            $db_connection->close();
+        }
+    }
+    catch(Exception $e)
+    {
+		echo '<span style="color:red;"> System niedostępny. Prosimy spróbować później/span>';
+		echo '<br />Erro code: '.$e;
+    }
+
+    function error()
+    {
+        $_SESSION['error'] = '<span style="color:red">Wprowadzono nieprawidłowy login lub hasło!</span>';
+        header('Location: ../index.php');
+    }
+
+    function loginUserLogged()
+    {
+        $user_logged_count = $result->num_rows;
+                if ($user_logged_count > 0)
                 {
-                    $_SESSION['isAdminLogged'] = true;
-                    $_SESSION['id'] = $row['ul.id'];
-                    $_SESSION['surname'] = $wiersz['surname'];
-                    $_SESSION['name'] = $wiersz['name'];
-                    $_SESSION['email'] = $wiersz['email'];
-                    unset($_SESSION['error']);
-                    $result->free_result();
-                    header('Location: adminLogged.html');
+                    $row = $rezultat->fetch_assoc();
+                    if (password_verify($password, $row['password']))
+                    {
+                        $_SESSION['isUserLogged'] = true;
+                        $_SESSION['id'] = $row['ul.id'];
+                        $_SESSION['surname'] = $wiersz['surname'];
+                        $_SESSION['name'] = $wiersz['name'];
+                        $_SESSION['email'] = $wiersz['email'];
+                        unset($_SESSION['error']);
+                        $result->free_result();
+                        header('Location: ../userLogged.html');
+                    }
+                    else
+                        error();
                 }
                 else
+                    error();
+    }
+
+    function loginAdmin()
+    {
+        $user_logged_count = $result->num_rows;
+                if ($user_logged_count > 0)
                 {
-                    $_SESSION['error'] = '<span style="color:red">Wprowadzono nieprawidłowy login lub hasło!</span>';
-                    header('Location: ../index.php');
+                    $row = $rezultat->fetch_assoc();
+                    if (password_verify($password, $row['password']))
+                    {
+                        $_SESSION['isAdminLogged'] = true;
+                        $_SESSION['id'] = $row['ul.id'];
+                        $_SESSION['surname'] = $wiersz['surname'];
+                        $_SESSION['name'] = $wiersz['name'];
+                        $_SESSION['email'] = $wiersz['email'];
+                        unset($_SESSION['error']);
+                        $result->free_result();
+                        header('Location: ../adminLogged.html');
+                    }
+                    else
+                        error();
                 }
-            }
-            else
-            {
-                $_SESSION['error'] = '<span style="color:red">Wprowadzono nieprawidłowy login lub hasło!</span>';
-                header('Location: ../index.php');
-            }
-        }
-        $db_connection->close();
+                else
+                    error();
     }
 ?>
