2e4c282536b7fd2bef4747cc736bdd39dc964a0c
Mariia Borowiec <syrotamaria@gmail.com>
2018-12-25 23:17:27 +0100

    Dodanie walidacji danych przy logowaniu

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,45 @@
 /*** POPUP LOGIN ***/
 document.getElementById('clickLogin').addEventListener('click', function () {
     document.querySelector('.bg-modal-login').style.display = 'flex';
+    const formLogin = new Vue(
+        {
+            el: '#form-login',
+            data: {
+                errorLogin: '',
+                errorPassword: '',
+                emailLogin: null,
+                passwordLogin: null
+            },
+            methods: {
+                checkForm: function (e) {
+                    this.errorLogin = '';
+                    this.errorPassword = '';
+                    if (!this.passwordLogin) {
+                        this.errorPassword = "Wprowadź hasło.";
+                    } else if (!this.validPassword(this.passwordLogin)) {
+                        this.errorPassword = "Niepoprawny format hasła.";
+                    }
+
+                    if (!this.emailLogin) {
+                        this.errorLogin = "Wprowadź email.";
+                    } else if (!this.validEmail(this.emailLogin)) {
+                        this.errorLogin = "Niepoprawny email.";
+                    }
+
+                    if (!this.errorLogin.length) return true;
+                    e.preventDefault();
+                    if (!this.errorPassword.length) return true;
+                    e.preventDefault();
+                },
+                validPassword: function (passwordLogin) {
+                    return passwordLogin.length > 8 && passwordLogin.length < 20;
+                },
+                validEmail: function (emailLogin) {
+                    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    return re.test(emailLogin);
+                }
+            }
+        });
 });
 document.querySelector('.close-login').addEventListener('click', function () {
     document.querySelector('.bg-modal-login').style.display = 'none';
@@ -27,6 +66,6 @@ function login() {
         window.open("/admin.html");
     }
     else {
-        
+
     }
 }
