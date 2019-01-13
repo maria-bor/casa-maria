2da2fa58b920b7acaec1e6ebe5aaf423e6c78cdd
Mariia Borowiec <syrotamaria@gmail.com>
2019-01-13 15:48:24 +0100

    Przeniesienie metod zmieniających imię, nazwisko i email użytkownika do osobnego pliku .js (export/import)

--- js/user.js
+++ js/user.js
@@ -1,4 +1,4 @@
-import changeProfile from './changeProfile.js';
+import {setupChangeNameHandler, setupChangeSurnameHandler, setupChangeEmailHandler} from './changeProfile.js';
 
 $(function () {
     $(".menu-toggle").on("click", function (e) {
@@ -61,80 +61,10 @@ $(function () {
     })
 });
 
-// CHANGE NAME ON PROFILE
-var changedName;
-$('#name-change-butt').on('click', function () {
-    if ($('#name-change-butt').val() === 'Zmień') {
-        changedName = $('#userName').val();
-        $('#userName').prop('disabled', false);
-        $('#name-change-butt').prop('value', 'Ok');
-    }
-    else if ($('#name-change-butt').val() === 'Ok') {
-        let newChangedName = $('#userName').val();
-        if (/^[a-zA-Z]{3,20}?$/.test(newChangedName)) {
-            $('#userName').prop('disabled', true);
-            $('#name-change-butt').prop('value', 'Zmień');
-            $('#errorName').empty();
-            if (changedName != newChangedName) {
-                changedName = newChangedName;
-                changeProfile("new_name", changedName, 'Name');
-            }
-        }
-        else {
-            $('#errorName').text('Niepoprawny format.');
-        }
-    }
-});
-
-// CHANGE SURNAME ON PROFILE
-var changedSurname;
-$('#surname-change-butt').on('click', function () {
-    if ($('#surname-change-butt').val() === 'Zmień') {
-        changedSurname = $('#userSurname').val();
-        $('#userSurname').prop('disabled', false);
-        $('#surname-change-butt').prop('value', 'Ok');
-    }
-    else if ($('#surname-change-butt').val() === 'Ok') {
-        let newChangedSurname = $('#userSurname').val();
-        if (/^[a-zA-Z]{3,20}?$/.test(newChangedSurname)) {
-            $('#userSurname').prop('disabled', true);
-            $('#surname-change-butt').prop('value', 'Zmień');
-            $('#errorSurname').empty();
-            if (changedSurname != newChangedSurname) {
-                changedSurname = newChangedSurname;
-                changeProfile("new_surname", changedSurname, 'Surname');
-            }
-        }
-        else {
-            $('#errorSurname').text('Niepoprawny format.');
-        }
-    }
-});
-
-// CHANGE EMAIL ON PROFILE
-var changedEmail;
-$('#email-change-butt').on('click', function () {
-    if ($('#email-change-butt').val() === 'Zmień') {
-        changedEmail = $('#userEmail').val();
-        $('#userEmail').prop('disabled', false);
-        $('#email-change-butt').prop('value', 'Ok');
-    }
-    else if ($('#email-change-butt').val() === 'Ok') {
-        let newChangedEmail = $('#userEmail').val();
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(newChangedEmail)) {
-            $('#userEmail').prop('disabled', true);
-            $('#email-change-butt').prop('value', 'Zmień');
-            $('#errorEmail').empty();
-            if (changedEmail != newChangedEmail) {
-                changedEmail = newChangedEmail;
-                changeProfile("new_email", changedEmail, 'Email');
-            }
-        }
-        else {
-            $('#errorEmail').text('Niepoprawny format.');
-        }
-    }
-});
+// ***USER DATA***
+$(setupChangeNameHandler());
+$(setupChangeSurnameHandler());
+$(setupChangeEmailHandler());
 
 // CHANGE PASSWORD ON PROFILE
 var changedPassword;
