e8fd0d5dc87b644c84bfc53028a9b1d1832f929a
Mariia Borowiec <syrotamaria@gmail.com>
2019-01-26 23:20:24 +0100

    Przelaczanie zakladek konto/zarejestruj sie/zaloguj sie/wyloguj sie

--- js/javascript.js
+++ js/javascript.js
@@ -1,47 +1,59 @@
+// Putting the logout function as a property on the window object
+window.logout = logout;
+function logout() {
+    window.location.replace("./php/logout.php");
+}
+
 /*** POPUP LOGIN ***/
 document.getElementById('clickLogin').addEventListener('click', function () {
-    document.querySelector('.bg-modal-login').style.display = 'flex';
-    const formLogin = new Vue(
-        {
-            el: '#form-login',
-            data: {
-                errorLogin: '',
-                errorPassword: '',
-                emailLogin: null,
-                passwordLogin: null
-            },
-            methods: {
-                checkForm: function (e) {
-                    this.errorLogin = '';
-                    this.errorPassword = '';
-
-                    if (!this.passwordLogin) {
-                        this.errorPassword = "Wprowadź hasło.";
-                    } else if (!this.validPassword(this.passwordLogin)) {
-                        this.errorPassword = "Niepoprawny format hasła.";
-                    }
-
-                    if (!this.emailLogin) {
-                        this.errorLogin = "Wprowadź email.";
-                    } else if (!this.validEmail(this.emailLogin)) {
-                        this.errorLogin = "Niepoprawny email.";
-                    }
-
-                    if (!this.errorLogin.length && !this.errorPassword.length) {
-                        return true;
-                    }
-                    e.preventDefault();
+    let label = document.getElementById('clickLogin').innerText;
+    console.log(label);
+    if (label === 'Zaloguj się') {
+        document.querySelector('.bg-modal-login').style.display = 'flex';
+        const formLogin = new Vue(
+            {
+                el: '#form-login',
+                data: {
+                    errorLogin: '',
+                    errorPassword: '',
+                    emailLogin: null,
+                    passwordLogin: null
                 },
-                validPassword: function (passwordLogin) {
-                    var pass = passwordLogin.trim();
-                    return (pass.length >= 8) && (pass.length <= 20);
-                },
-                validEmail: function (emailLogin) {
-                    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-                    return mailFormat.test(emailLogin);
+                methods: {
+                    checkForm: function (e) {
+                        this.errorLogin = '';
+                        this.errorPassword = '';
+
+                        if (!this.passwordLogin) {
+                            this.errorPassword = "Wprowadź hasło.";
+                        } else if (!this.validPassword(this.passwordLogin)) {
+                            this.errorPassword = "Niepoprawny format hasła.";
+                        }
+
+                        if (!this.emailLogin) {
+                            this.errorLogin = "Wprowadź email.";
+                        } else if (!this.validEmail(this.emailLogin)) {
+                            this.errorLogin = "Niepoprawny email.";
+                        }
+
+                        if (!this.errorLogin.length && !this.errorPassword.length) {
+                            return true;
+                        }
+                        e.preventDefault();
+                    },
+                    validPassword: function (passwordLogin) {
+                        var pass = passwordLogin.trim();
+                        return (pass.length >= 8) && (pass.length <= 20);
+                    },
+                    validEmail: function (emailLogin) {
+                        var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                        return mailFormat.test(emailLogin);
+                    }
                 }
-            }
-        });
+            });
+    } else if (label === 'Wyloguj się') {
+        logout();
+    }
 });
 document.querySelector('.close-login').addEventListener('click', function () {
     document.querySelector('.bg-modal-login').style.display = 'none';
@@ -49,87 +61,93 @@ document.querySelector('.close-login').addEventListener('click', function () {
 
 /*** POPUP REGISTER ***/
 document.getElementById('clickRegister').addEventListener('click', function () {
-    document.querySelector('.bg-modal-register').style.display = 'flex';
-    const formRegister = new Vue(
-        {
-            el: '#form-register',
-            data: {
-                errorName: '',
-                errorSurname: '',
-                errorEmail: '',
-                errorPassword: '',
-                errorPasswordConfirm: '',
+    let label = document.getElementById('clickRegister').innerText;
+    console.log(label);
+    if (label === 'Zarejestruj się') {
+        document.querySelector('.bg-modal-register').style.display = 'flex';
+        const formRegister = new Vue(
+            {
+                el: '#form-register',
+                data: {
+                    errorName: '',
+                    errorSurname: '',
+                    errorEmail: '',
+                    errorPassword: '',
+                    errorPasswordConfirm: '',
 
-                nameRegister: null,
-                surnameRegister: null,
-                email: null,
-                passwordRegister: null,
-                passwordConfirm: null
-            },
-            methods: {
-                checkForm: function (e) {
-                    this.errorName = '';
-                    this.errorSurname = '';
-                    this.errorEmail = '';
-                    this.errorPassword = '';
-                    this.errorPasswordConfirm = '';
-
-                    if (!this.nameRegister) {
-                        this.errorName = "Wprowadź imię.";
-                    } else if (!this.validNameSurname(this.nameRegister)) {
-                        this.errorName = "Niepoprawny format.";
-                    }
-
-                    if (!this.surnameRegister) {
-                        this.errorSurname = "Wprowadź nazwisko.";
-                    } else if (!this.validNameSurname(this.surnameRegister)) {
-                        this.errorSurname = "Niepoprawny format.";
-                    }
-
-                    if (!this.email) {
-                        this.errorEmail = "Wprowadź email.";
-                    } else if (!this.validEmail(this.email)) {
-                        this.errorEmail = "Niepoprawny email.";
-                    }
-
-                    if (!this.passwordRegister) {
-                        this.errorPassword = "Wprowadź hasło.";
-                    } else if (!this.validPassword(this.passwordRegister)) {
-                        this.errorPassword = "Niepoprawny format hasła. Min 8 znaków.";
-                    }
-
-                    if (!this.passwordConfirm) {
-                        this.errorPasswordConfirm = "Wprowadź hasło.";
-                    } else if (!this.validPasswordConfirm(this.passwordConfirm)) {
-                        this.errorPasswordConfirm = "Hasła nie zgadzają się.";
-                    }
-
-                    if (!this.errorName.length && !this.errorSurname.length
-                        && !this.errorEmail.length && !this.errorPassword.length
-                        && !this.errorPasswordConfirm.length) {
-                        return true;
-                    }
-                    e.preventDefault();
+                    nameRegister: null,
+                    surnameRegister: null,
+                    email: null,
+                    passwordRegister: null,
+                    passwordConfirm: null
                 },
-                validNameSurname: function (str) {
-                    var nameOrSurname = str.trim();
-                    var nameOrSurnameFormat = /^[a-zA-Z]{3,20}?$/;
-                    return nameOrSurnameFormat.test(nameOrSurname);
-                },
-                validEmail: function (email) {
-                    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-                    return mailFormat.test(email);
-                },
-                validPassword: function (passwordRegister) {
-                    var pass = passwordRegister.trim();
-                    return (pass.length >= 8) && (pass.length <= 20);
-                },
-                validPasswordConfirm: function (passwordConfirm) {
-                    var passConfirm = passwordConfirm.trim();
-                    return passConfirm === this.passwordRegister;
+                methods: {
+                    checkForm: function (e) {
+                        this.errorName = '';
+                        this.errorSurname = '';
+                        this.errorEmail = '';
+                        this.errorPassword = '';
+                        this.errorPasswordConfirm = '';
+
+                        if (!this.nameRegister) {
+                            this.errorName = "Wprowadź imię.";
+                        } else if (!this.validNameSurname(this.nameRegister)) {
+                            this.errorName = "Niepoprawny format.";
+                        }
+
+                        if (!this.surnameRegister) {
+                            this.errorSurname = "Wprowadź nazwisko.";
+                        } else if (!this.validNameSurname(this.surnameRegister)) {
+                            this.errorSurname = "Niepoprawny format.";
+                        }
+
+                        if (!this.email) {
+                            this.errorEmail = "Wprowadź email.";
+                        } else if (!this.validEmail(this.email)) {
+                            this.errorEmail = "Niepoprawny email.";
+                        }
+
+                        if (!this.passwordRegister) {
+                            this.errorPassword = "Wprowadź hasło.";
+                        } else if (!this.validPassword(this.passwordRegister)) {
+                            this.errorPassword = "Niepoprawny format hasła. Min 8 znaków.";
+                        }
+
+                        if (!this.passwordConfirm) {
+                            this.errorPasswordConfirm = "Wprowadź hasło.";
+                        } else if (!this.validPasswordConfirm(this.passwordConfirm)) {
+                            this.errorPasswordConfirm = "Hasła nie zgadzają się.";
+                        }
+
+                        if (!this.errorName.length && !this.errorSurname.length
+                            && !this.errorEmail.length && !this.errorPassword.length
+                            && !this.errorPasswordConfirm.length) {
+                            return true;
+                        }
+                        e.preventDefault();
+                    },
+                    validNameSurname: function (str) {
+                        var nameOrSurname = str.trim();
+                        var nameOrSurnameFormat = /^[a-zA-Z]{3,20}?$/;
+                        return nameOrSurnameFormat.test(nameOrSurname);
+                    },
+                    validEmail: function (email) {
+                        var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                        return mailFormat.test(email);
+                    },
+                    validPassword: function (passwordRegister) {
+                        var pass = passwordRegister.trim();
+                        return (pass.length >= 8) && (pass.length <= 20);
+                    },
+                    validPasswordConfirm: function (passwordConfirm) {
+                        var passConfirm = passwordConfirm.trim();
+                        return passConfirm === this.passwordRegister;
+                    }
                 }
-            }
-        });
+            });
+    } else if (label === 'Konto') {
+        window.location.replace("./user.html");
+    }
 });
 document.querySelector('.close-register').addEventListener('click', function () {
     document.querySelector('.bg-modal-register').style.display = 'none';
