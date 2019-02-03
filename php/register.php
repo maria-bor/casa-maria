ca339430501a4cba387867450c80f7cecf223c47
Mariia Borowiec <syrotamaria@gmail.com>
2019-02-03 04:47:57 +0100

    Zmiana rejestracji

--- php/register.php
+++ php/register.php
@@ -49,93 +49,140 @@
 
         try 
         {
-            // Sprawdzamy czy nie ma juz konta dla podanego emaila (rola nie wazna):
-            $sql = 'SELECT u.idUser id_user
-                    FROM User u
-                    INNER JOIN userlogged ul
-                    ON ul.idUser = u.idUser
-                    WHERE u.email = :emailLogin
-                    AND ul.isDeleted = 0;';
-            $query = $db->prepare($sql);
-            $query->bindValue(':emailLogin', $email, PDO::PARAM_STR);
-            $query->execute();
-            if ($query->rowCount() > 0) {
-                $all_valid = false;
-                $_SESSION['error_email'] = 'Konto o podanym email już istnieje!';
-            }
-
             if ($all_valid) {
-                // Wyciągnięcie roli jeszcze przed transakcją
-                $sql = 'SELECT idRole
-                        FROM Role r
-                        WHERE r.name = :user_role
-                        LIMIT 1;';
+                // Sprawdzamy czy nie ma już konta aktywnego dla podanego emaila (rola nie ważna):
+                $sql = 'SELECT u.idUser id_user
+                FROM User u
+                INNER JOIN userlogged ul
+                ON ul.idUser = u.idUser
+                WHERE u.email = :emailLogin
+                AND ul.activated = 1
+                AND ul.isDeleted = 0;';
                 $query = $db->prepare($sql);
-                $role_name = '';
-                if (isset($_SESSION['is_admin_logged'])) {
-                    $role_name = 'admin';
+                $query->bindValue(':emailLogin', $email, PDO::PARAM_STR);
+                $query->execute();
+                if ($query->rowCount() > 0) {
+                    $all_valid = false;
+                    $_SESSION['error_email'] = 'Konto już istnieje!';
+                    header('Location: ../index.html');
+                    exit();
                 } else {
-                    $role_name = 'user_logged';
+                    // Sprawdzamy czy jest konto ale nie aktywne:
+                    $sql = 'SELECT u.idUser id_user
+                        FROM User u
+                        INNER JOIN userlogged ul
+                        ON ul.idUser = u.idUser
+                        WHERE u.email = :emailLogin
+                        AND ul.activated = 0
+                        AND ul.isDeleted = 0;';
+                    $query = $db->prepare($sql);
+                    $query->bindValue(':emailLogin', $email, PDO::PARAM_STR);
+                    $query->execute();
+                    if ($query->rowCount() > 0) {
+                        $all_valid = false;
+                        $_SESSION['error_email'] =  'Konto nieaktywne!'.
+                                                    'Aktywuj je klikając w link wysłany na adres podany przy rejestracji.';
+                        header('Location: ../index.html');
+                        exit();
+                    } else {
+
+                        // Tworzymy wiadomość email:
+                        $code = substr(md5(mt_rand()),0,15);
+                        $verification_link = "http://localhost/casa-maria/php/activate.php?code=".$code."&email=".urlencode($email);
+                        $htmlStr = "";
+                        $htmlStr .= "Dzień dobry " . $email . ",<br /><br />";
+
+                        $htmlStr .= "Kliknij w poniższy link w celu aktywacji swojego konta, dzięki czemu będziesz mógł dokonać rezeracji w systemie <b>CASA-MARIA</b>.<br /><br /><br />";
+                        $htmlStr .= "<a href='{$verification_link}' target='_blank' style='padding:1em; font-weight:bold; background-color:blue; color:#fff;'>AKTYWUJ EMAIL</a><br /><br /><br />";
+
+                        $htmlStr .= "Pozdrawiamy,<br />";
+                        $htmlStr .= "<a href='http://localhost/casa-maria/' target='_blank'>CASA-MARIA</a><br />";
+
+                        $from = "CASA-MARIA";
+                        $email_sender = "kitdlainnychspam@gmail.com";
+                        $subject = "Link aktywacyjny | CASA-MARIA";
+                        $recipient_email = $email;
+
+                        $headers  = "MIME-Version: 1.0\r\n";
+                        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+                        $headers .= "From: {$from} <{$email_sender}> \n";
+
+                        $body = $htmlStr;
+
+                        // Wysłanie emaila
+                        if( mail($recipient_email, $subject, $body, $headers) ) {
+                            $_SESSION['msg'] = 'Link aktywacyjny zostało wysłany na adres <b>'.$email.
+                                            '</b> Prosimy otwórz swoją skrzynkę mailową i aktywuj swoje konto.';
+
+                            // Wyciągnięcie roli jeszcze przed transakcją
+                            $sql = 'SELECT idRole
+                                    FROM Role r
+                                    WHERE r.name = :user_role
+                                    LIMIT 1;';
+                            $query = $db->prepare($sql);
+                            $role_name = '';
+                            if (isset($_SESSION['is_admin_logged'])) {
+                                $role_name = 'admin';
+                            } else {
+                                $role_name = 'user_logged';
+                            }
+                            $query->bindValue(':user_role', $role_name, PDO::PARAM_STR);
+                            $query->execute();
+                            $result = $query->fetch();
+                            $id_role = $result['idRole'];
+
+                            $db->beginTransaction();
+
+                            // Wstawienie do tabeli User:
+                            $sql = 'INSERT INTO User
+                                    VALUES (
+                                    NULL,
+                                    :surname,
+                                    :name,
+                                    :email
+                                    );';
+                            $query = $db->prepare($sql);
+                            $query->bindValue(':surname', $surname, PDO::PARAM_STR);
+                            $query->bindValue(':name', $name, PDO::PARAM_STR);
+                            $query->bindValue(':email', $email, PDO::PARAM_STR);
+                            $query->execute();
+                            $id_user = $db->lastInsertId();
+
+                            // Wstawienie do tabeli UserLogged:
+                            $sql = 'INSERT INTO UserLogged (
+                                    idUserLogged, idUser, password, activated,
+                                    activation_code, created_at, modified_at)
+                                    VALUES (
+                                        NULL,
+                                        :id_user,
+                                        :password_hash,
+                                        :activated,
+                                        :code,
+                                        CURRENT_TIMESTAMP,
+                                        CURRENT_TIMESTAMP);';
+                            $query = $db->prepare($sql);
+                            $query->bindValue(':id_user', $id_user, PDO::PARAM_INT);
+                            $query->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+                            $query->bindValue(':activated', 0, PDO::PARAM_INT);
+                            $query->bindValue(':code', $code, PDO::PARAM_INT);
+                            $query->execute();
+
+                            // Wstawienie do tabeli UserRole:
+                            $sql = 'INSERT INTO UserRole
+                                    VALUES (:id_user_logged, :id_role);';
+                            $query = $db->prepare($sql);
+                            $query->bindValue(':id_user_logged', $db->lastInsertId(), PDO::PARAM_INT);
+                            $query->bindValue(':id_role', $id_role, PDO::PARAM_INT);
+                            $query->execute();
+
+                            $db->commit();
+
+                        } else {
+                            $_SESSION['error_register'] = true;
+                        }
+                        header('Location: ../index.html');
+                    }
                 }
-                $query->bindValue(':user_role', $role_name, PDO::PARAM_STR);
-                $query->execute();
-                $result = $query->fetch();
-                $id_role = $result['idRole'];
-
-                $db->beginTransaction();
-
-                // Wstawienie do tabeli User:
-                $sql = 'INSERT INTO User
-                        VALUES (
-                        NULL,
-                        :surname,
-                        :name,
-                        :email
-                        );';
-                $query = $db->prepare($sql);
-                $query->bindValue(':surname', $surname, PDO::PARAM_STR);
-                $query->bindValue(':name', $name, PDO::PARAM_STR);
-                $query->bindValue(':email', $email, PDO::PARAM_STR);
-                $query->execute();
-                $id_user = $db->lastInsertId();
-
-                // Wstawienie do tabeli UserLogged:
-                $sql = 'INSERT INTO UserLogged (idUserLogged, password, idUser)
-                        VALUES (
-                            NULL,
-                            :password_hash,
-                            :id_user);';
-                $query = $db->prepare($sql);
-                $query->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
-                $query->bindValue(':id_user', $id_user, PDO::PARAM_STR);
-                $query->execute();
-
-                // Wstawienie do tabeli UserRole:
-                $sql = 'INSERT INTO UserRole
-                        VALUES (:id_user_logged, :id_role);';
-                $query = $db->prepare($sql);
-                $query->bindValue(':id_user_logged', $db->lastInsertId(), PDO::PARAM_STR);
-                $query->bindValue(':id_role', $id_role, PDO::PARAM_STR);
-                $query->execute();
-
-                $db->commit();
-            
-                // Zapamietujemy dane rejestrujacego( a potem logujacego):
-                if (!isset($_SESSION['is_admin_logged'])) {
-                    $_SESSION['id_user'] = $id_user;
-                    $_SESSION['surname'] = $surname;
-                    $_SESSION['name'] = $name;
-                    $_SESSION['email'] = $email;
-                    $_SESSION['id_role'] = $id_role;
-                    $_SESSION['role_name'] = $role_name;
-                    $_SESSION['is_user_logged'] = true;
-                    $_SESSION['registered'] = true;
-
-                    header('Location: ../user.html');
-                } else {
-                    header('Location: ../admin.html');
-                }
-                
             } else {
                 $_SESSION['error_register'] = true;
                 header('Location: ../index.html');
